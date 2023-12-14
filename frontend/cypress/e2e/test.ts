27f8676197801681d5a6f5877ac7d9b9461faa94
MalinAlm <malin.therese.almqvist@gmail.com>
2023-12-14 14:03:15 +0100

    BDD test update click

--- frontend/cypress/e2e/test.ts
+++ frontend/cypress/e2e/test.ts
@@ -9,8 +9,13 @@ Before(() => {
   cy.visit("http://localhost:5173/");
 });
 
-Given("Jag är på hemsidan och ser en kalender", () => {});
+Given("Jag är på hemsidan och ser en kalender", () => {
+  cy.get("#calendar").should("be.visible");
+});
 
-When("Jag klickar på ett datum", () => {});
+When("Jag klickar på ett datum", () => {
+  cy.get("#pickDate").click();
+  cy.contains("data-handler='selectDay' a").click();
+});
 
 Then("Datumet markeras", () => {});
