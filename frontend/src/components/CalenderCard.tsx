fc3ed77434488021eb1f104f99d1efd43d2355e8
MalinAlm <malin.therese.almqvist@gmail.com>
2023-12-14 14:28:38 +0100

    added home page and calendar

---
+++ frontend/src/components/CalenderCard.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+function MyApp() {
+  const [value, onChange] = useState<Value>(new Date());
+
+  return (
+    <div>
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
+}
+export default MyApp;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import CalenderCard from "../components/CalenderCard";
+
+function Home() {
+  return (
+    <div>
+      <CalenderCard />
+    </div>
+  );
+}
+
+export default Home;
