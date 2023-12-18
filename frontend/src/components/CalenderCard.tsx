2fd6a6531a0bef2455c766c1864cddf58f2f30cf
MalinAlm <malin.therese.almqvist@gmail.com>
2023-12-18 20:59:04 +0100

    mark date and styling

--- frontend/src/components/CalenderCard.tsx
+++ frontend/src/components/CalenderCard.tsx
@@ -1,16 +1,32 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
-
+import "react-calendar/dist/Calendar.css";
+import "./CalenadrCard.css";
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 function MyApp() {
-  const [value, onChange] = useState<Value>(new Date());
+  const [selectedDate, setSelectedDate] = useState<Value>(new Date());
+  const markDateClassName: (props: calendarTileProperties) => string = ({
+    date,
+  }) => {
+    if (date && date.getTime() === (selectedDate as Date).getTime()) {
+      return "selected-date";
+    }
+    return "";
+  };
+  // const [value, onChange] = useState<Value>(new Date());
+  // const [selectedDate, setSelectedDate] = useState<Date | Date[]>(new Date());
 
   return (
     <div id="calendar">
-      <Calendar className="pickDate" onChange={onChange} value={value} />
+      <Calendar
+        className="pickDate"
+        onChange={(date) => setSelectedDate(date as Value)}
+        value={selectedDate as Value}
+        tileClassName={markDateClassName}
+      />
     </div>
   );
 }
