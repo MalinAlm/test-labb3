503bff4e4d690a17a98710156701f08c444032d4
MalinAlm <malin.therese.almqvist@gmail.com>
2024-01-12 15:58:56 +0100

    fetch workout journal

--- frontend/src/pages/Journal.tsx
+++ frontend/src/pages/Journal.tsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+import "../App.css";
+
+interface WorkoutData {
+  id?: number;
+  date: string;
+  workout: string;
+  notes: string;
+}
+
 function Journal() {
+  const [workoutData, setWorkoutData] = useState<WorkoutData[]>([
+    { workout: "", date: "", notes: "" },
+  ]);
+
+  useEffect(() => {
+    console.log("Fetching data from /journal...");
+    fetch("/api/journal")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((result: WorkoutData[]) => {
+        console.log("Result:", result);
+        setWorkoutData(result);
+        console.log("workoutData:", workoutData);
+      })
+      .catch((error) => {
+        console.error("Error fetching Journaaal", error);
+      });
+  }, []);
+  console.log("Rendered workoutData:", workoutData);
+
   return (
     <div>
-      <h1>Journal</h1>
+      <h1>My Journal</h1>
+      <div>
+        {workoutData.map((workout) => (
+          <div id="renderDiv" key={workout.id}>
+            <p>{workout.date}</p>
+            <p>{workout.workout}</p>
+            <p>{workout.notes}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
